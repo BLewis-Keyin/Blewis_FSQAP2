6f503b3c5e657d5be3bffab8fc9f3c1471c0f32a
BLewis <45450044+BLewis-Keyin@users.noreply.github.com>
2023-10-17 05:27:57 +0000

    Clean up Code clean up

--- app.js
+++ app.js
@@ -5,58 +5,70 @@ const fs = require('fs');
 const path = require('path');
 const console = require('console');
 
-//Set up eventEmitter
+//eventEmitter
 const eventEmitter = new events.EventEmitter();
+eventEmitter.on('request', (req) => {
+    console.log(`Received request for: ${req.url}`);
+})
 
+//Activity Log
 const logFileStream = fs.createWriteStream('server.log', { flags: 'a' });
 
 function logActivity(message) {
     const timestamp = new Date().toUTCString();
     const logMessage = `[${timestamp}] ${message}`;
-
     console.log(logMessage); // Log to the console
     logFileStream.write(logMessage + '\n'); // Log to the file
 }
 
+//Server Setup
 
-//Set up server
+
 const server = http.createServer((req, res) => {
+    const loggedRequests = new Set();
     const url = req.url;
 
-    logActivity(`Received request for: ${url}`);
+    if (loggedRequests.has(url)) {} else {
 
-    const baseDirectory = path.join(__dirname, 'views');
-    const filePath = path.join(baseDirectory, url === '/' ? 'index.html' : `${url}.html`);
+        logActivity(`Received request for: ${url}`);
+        loggedRequests.add(url);
 
-    fs.access(filePath, fs.constants.F_OK, (err) => {
-        if (err) {
-            // File does not exist
-            if (url === "/styles.css") {
-                // Do nothing, not needed if browser requests styles.css
-                res.writeHead(404, { 'Content-Type': 'text/plain' });
-                res.end('URL Not Found');
-            } else {
-                logActivity(`URL Not Found for: ${url}`);
-                res.writeHead(404, { 'Content-Type': 'text/plain' });
-                res.end('URL Not Found');
-            }
-        } else {
-            // File exists
-            fs.readFile(filePath, 'utf8', (err, data) => {
-                if (err) {
-                    // An error occurred while reading the file
-                    logActivity(`Error reading file for: ${url}`);
-                    res.writeHead(500, { 'Content-Type': 'text/plain' });
-                    res.end('Internal Server Error');
+        const baseDirectory = path.join(__dirname, 'views');
+        const filePath = path.join(baseDirectory, url === '/' ? 'index.html' : `${url}.html`);
+
+        fs.access(filePath, fs.constants.F_OK, (err) => {
+            if (err) {
+                // File does not exist
+                if (url === "/styles.css") {
+                    // Do nothing, not needed if browser requests styles.css
+                    res.writeHead(404, { 'Content-Type': 'text/plain' });
+                    res.end('URL Not Found');
                 } else {
-                    // Successful, serve the HTML content
-                    logActivity(`Served HTML file for: ${url}`);
-                    res.writeHead(200, { 'Content-Type': 'text/html' });
-                    res.end(data);
+                    logActivity(`URL Not Found for: ${url}`);
+                    res.writeHead(404, { 'Content-Type': 'text/plain' });
+                    res.end('URL Not Found');
                 }
-            });
-        }
-    });
+            } else {
+                // File exists
+                fs.readFile(filePath, 'utf8', (err, data) => {
+                    if (err) {
+                        // An error occurred while reading the file
+                        logActivity(`Error reading file for: ${url}`);
+                        res.writeHead(500, { 'Content-Type': 'text/plain' });
+                        res.end('Internal Server Error');
+                    } else {
+                        // Successful, serve the HTML content
+                        logActivity(`Served HTML file for: ${url}`);
+                        res.writeHead(200, { 'Content-Type': 'text/html' });
+                        res.end(data);
+                    }
+                });
+            }
+        })
+    };
+});
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
 });
 
 // switch (url) {
@@ -80,23 +92,3 @@ const server = http.createServer((req, res) => {
 //         res.writeHead(404, { 'Content-Type': 'text/plain' });
 //         res.end('Not Found');
 // };
-
-server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
-
-
-function serveHTMLFile(filename, res) {
-    fs.readFile(filename, 'utf8', (err, data) => {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Internal Server Error');
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data);
-        }
-    });
-}
-eventEmitter.on('request', (req) => {
-    console.log(`Received request for: ${req.url}`);
-});
